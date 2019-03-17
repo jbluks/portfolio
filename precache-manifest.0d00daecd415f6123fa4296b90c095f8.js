0e9ad14379859a72853898fef1dd5f148a298166
Jonathan <jon.bluks@gmail.com>
2019-03-17 00:35:27 -0700

    Updates

---
+++ precache-manifest.0d00daecd415f6123fa4296b90c095f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5e057ad57057a882eba6",
+    "url": "/static/js/main.c323c450.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5e057ad57057a882eba6",
+    "url": "/static/css/main.b91b4750.chunk.css"
+  },
+  {
+    "revision": "a18ff6e475c328e83429ed3baffe7f82",
+    "url": "/index.html"
+  }
+];
